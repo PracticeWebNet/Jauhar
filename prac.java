4cd27c590bc7eb619719449f04e774726266c698
Shubham <shubham@webnettechnology.in>
2025-04-03 16:06:03 +0530

    add jdbc code

--- prac.java
+++ prac.java
@@ -1,5 +1,15 @@
+import java.sql.*;
 class Prac{
-    public static void main(String [] args){
-        System.out.println("hello");
+    public static void main(String [] args) throws Exception{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager
+        .getConnection("jdbc:mysql://localhost:3306/prac", "root", "root");
+        Statement st = con.createStatement();
+        int n  = st.executeUpdate("create table emp(id number, ename varchar(10), )");
+        System.out.println(n);
+
+    }
+    public static void h(){
+
     }
 }
